6793db5d641b4f3d35e0d70ebd6cba9f259fef4b
bessem hadj ali <bessem.hadj.ali89@gmail.com>
2020-10-11 17:24:26 +0200

    Initial Commit

---
+++ EventsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventsApp
+//
+//  Created by Bessem Hadj Ali on 11/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
